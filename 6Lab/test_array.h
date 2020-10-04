5901b487820560c5ea65f878a489f393f1062130
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2020-10-04 22:25:52 +0300

    added header for custom std::array test methods

---
+++ 6Lab/test_array.h
@@ -0,0 +1,12 @@
+//
+// Created by yuriy on 28.07.20.
+//
+
+#ifndef CONTAINERS_TEST_ARRAY_H
+#define CONTAINERS_TEST_ARRAY_H
+
+namespace array_test {
+    void run();
+}
+
+#endif //CONTAINERS_TEST_ARRAY_H
